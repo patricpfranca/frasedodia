e158a6a1ef98421211a588118f7704859a4dd44c
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-07 23:36:16 -0200

    adicionando imagens

--- index.android.js
+++ index.android.js
@@ -1,38 +1,24 @@
 import React from 'react';
-import { Text, AppRegistry, View, TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image } from 'react-native';
 
 const Estilos = {
   principal: {
     paddingTop: 40
   },
-  botao: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderColor: '#1d8eb8',
-    borderWidth: 1,
-    borderRadius: 8,
-    elevation: 3
-  },
-  textoBotao: {
-    color: '#48bbec',
-    fontSize: 16,
-    fontWeight: 'bold',
-    alignSelf: 'center'
+  imagem: {
+    justifyContent: 'flex-end',
+    padding: 5
   }
 }
 
-const botaoPressionado = () => {
-  alert('Botão pressionado');
-}
-
 const App = () => {
-  const { principal, botao, textoBotao } = Estilos;
+  const { principal, imagem } = Estilos;
 
   return (
     <View style={ principal }>
-      <TouchableOpacity style={botao}>
-        <Text style={textoBotao}>Clique aqui</Text>
-      </TouchableOpacity>
+      <Image source={ require('./imgs/uvas.png') } >
+        <Text style={ imagem }>Legenda para a foto</Text>
+      </Image>
     </View>
   );
 };
