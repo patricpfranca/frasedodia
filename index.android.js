36cb840ae9a4af37d213db9181fcc8428664f974
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-07 23:21:04 -0200

    customizando botões parte II

--- index.android.js
+++ index.android.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import { Text, AppRegistry, View, Button } from 'react-native';
+import { Text, AppRegistry, View, TouchableOpacity } from 'react-native';
 
 const Estilos = {
   principal: {
     paddingTop: 40
+  },
+  botao: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderColor: '#1d8eb8',
+    borderWidth: 1,
+    borderRadius: 8,
+    elevation: 3
+  },
+  textoBotao: {
+    color: '#48bbec',
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center'
   }
 }
 
-const botapPressionado = () => {
+const botaoPressionado = () => {
   alert('Botão pressionado');
 }
 
 const App = () => {
-  const { principal } = Estilos;
+  const { principal, botao, textoBotao } = Estilos;
 
   return (
     <View style={ principal }>
-      <Button 
-        onPress={botapPressionado}
-        title="Clique aqui"
-        color="#841584"
-        accessibilityLabel="Clique aqui para abrir as notícias!"
-      />
+      <TouchableOpacity style={botao}>
+        <Text style={textoBotao}>Clique aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 };
