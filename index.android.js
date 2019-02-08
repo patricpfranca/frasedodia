30be201119be0423602152ada182c0844f89b980
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-07 23:48:35 -0200

    layout app frase do dia

--- index.android.js
+++ index.android.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import { Text, AppRegistry, View, Image } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity } from 'react-native';
 
 const Estilos = {
   principal: {
-    paddingTop: 40
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  imagem: {
-    justifyContent: 'flex-end',
-    padding: 5
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 20
+  },
+  textoBotao: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
   }
 }
 
+const botaoPressionado = () => {
+
+}
+
 const App = () => {
-  const { principal, imagem } = Estilos;
+  const { principal, botao, textoBotao } = Estilos;
 
   return (
     <View style={ principal }>
-      <Image source={ require('./imgs/uvas.png') } >
-        <Text style={ imagem }>Legenda para a foto</Text>
-      </Image>
+      <Image source={require('./imgs/logo.png')} />
+      <TouchableOpacity style={ botao }>
+        <Text style={ textoBotao }>Nova frase</Text>
+      </TouchableOpacity>
     </View>
   );
 };
